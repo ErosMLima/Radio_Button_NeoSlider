8ae848074f0e8222e429786a3b71200c1a37dfba
Eros M.Lima <60955355+ErosMLima@users.noreply.github.com>
2021-08-02 14:05:32 -0300

    Radio Button NeoSlider [001]
    Radio Button NeoSlider [001]

---
+++ script.js
@@ -0,0 +1,54 @@
+const texts = ["website", "react apps"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+  if (count === texts.length) {
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currentText.length) {
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 280);
+})();
+
+
+
+
+
+
+const text = document.querySelector(".fancy");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for (let i = 0; i < splitText.length; i++) {
+  text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+
+let timer = setInterval(onTick, 100);
+
+function onTick() {
+  const span = text.querySelectorAll("span")[char];
+  span.classList.add("fade");
+  char++;
+  if (char === splitText.length) {
+    complete();
+    return;
+  }
+}
+
+function complete() {
+  clearInterval(timer);
+  timer = null;
+}
